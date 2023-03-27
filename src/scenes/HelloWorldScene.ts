a33dc0c820d82431a974efb14d14f6205dca207e
Militsa Tuseeva <m.tuseeva@timeweb.ru>
2023-03-27 23:29:57 +0300

    feat: add moves and anims

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -3,6 +3,8 @@ import Phaser from 'phaser'
 export default class HelloWorldScene extends Phaser.Scene
 {
     private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
     
 	constructor()
 	{
@@ -26,9 +28,56 @@ export default class HelloWorldScene extends Phaser.Scene
         (<Phaser.Physics.Arcade.Sprite>this.platforms.create(400, 568, 'ground'))
             .setScale(2)
             .refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNames('dude', {
+                start: 0, end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNames('dude', {
+                start: 5, end: 8}
+            ),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+        if (this.cursors?.left?.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play('left', true);
+        } else if (this.cursors?.right?.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play('right', true);
+        } else {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play('turn', true);
+        }
 
+        if (this.cursors?.up?.isDown && this.player?.body.touching.down) {
+            this.player?.setVelocityY(-330);
+        }
     }
 }
